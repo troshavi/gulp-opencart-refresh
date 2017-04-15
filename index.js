de26fdb71cdb2bfc7c7355818757a1d5ecfd1535
dean992008 <dean99200835@gmail.com>
2017-04-15 22:41:45 +0300

    version 0.0.1b - Created script Opencart 2.3.x refresh.

---
+++ index.js
@@ -0,0 +1,54 @@
+const request = require('request');
+const through = require('through2').obj;
+const gutil = require('gulp-util');
+const getQueryParam = require('get-query-param');
+
+var options;
+
+function getKey(){
+    return new Promise((resolve, reject) => {
+        var key;
+        return request({
+            url: options.url + '/admin/index.php?route=common/login',
+            method: 'POST',
+            form: {
+                username: options.login,
+                password: options.password
+            }
+        }).on('response', (response)=>{
+            resolve(getQueryParam('token', response.headers.location));
+        });
+    });
+}
+
+function refreshCache(key){
+    return new Promise((resolve, reject) => {
+        request.get(options.url + '/admin/index.php?route=extension/modification/refresh&token=' + key)
+        .on('response', (response)=>{
+            resolve(true);
+        });
+    });
+}
+
+function opencartRefresh(setting) {
+    if (!options.url) {
+        throw new gutil.PluginError('gulp-opencart-refresh', '`url` is required!');
+    }
+    if (!options.login) {
+        throw new gutil.PluginError('gulp-opencart-refresh', '`login` is required!');
+    }
+    if (!options.password) {
+        throw new gutil.PluginError('gulp-opencart-refresh', '`password` is required!');
+    }
+    options = setting;
+    getKey()
+        .then(refreshCache)
+        .then((result)=>{
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+    return through(function (file, encoding, callback) { });
+}
+
+module.exports = opencartRefresh;
