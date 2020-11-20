e156c135c39f8e139e2f96a1ef7ad7419a0117b4
troshavi <troshavi@bk.ru>
2020-11-20 23:04:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const request = require('request'); //.defaults({'proxy':'http://127.0.0.1:8080'});
 const through = require('through2').obj;
 const gutil = require('gulp-util');
 const getQueryParam = require('get-query-param');
@@ -7,26 +7,32 @@ let options;
 
 function getKey() {
     return new Promise((resolve, reject) => {
-        var key;
         request({
             url: options.url + '/admin/index.php?route=common/login',
             method: 'POST',
-            form: {
+            formData: {
                 username: options.login,
                 password: options.password
             }
         })
             .on('response', (response) => {
-                resolve(getQueryParam('token', response.headers.location));
+                resolve({
+                    user_token: getQueryParam('user_token', response.headers.location),
+                    cookie: response.headers['set-cookie']
+                });
             });
     });
 }
 
-function refreshCache(key) {
+function refreshCache(sesion) {
+    console.log(sesion);
     return new Promise((resolve, reject) => {
         request({
-            url: options.url + '/admin/index.php?route=extension/modification/refresh&token=' + key,
-            method: 'GET'
+            url: options.url + '/admin/index.php?route=marketplace/modification/refresh&user_token=' + sesion.user_token,
+            method: 'GET',
+            headers: {
+                'Cookie': sesion.cookie
+            }
         })
             .on('response', (response) => {
                 resolve(true);
@@ -49,7 +55,7 @@ function opencartRefresh(setting) {
         .then(refreshCache)
         .then((result) => {
             const time = new Date();
-            const now = `[${time.getHours}:${time.getMinutes}:${time.setSeconds}] `;
+            const now = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] `;
             (result) ? console.log(now + 'Cache was cleared') : console.log(now + 'Something wrong');
         })
         .catch((error) => {
